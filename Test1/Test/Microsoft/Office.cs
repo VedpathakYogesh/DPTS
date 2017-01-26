3d97299e09a449cbdf7b19f21698b0d47149a358
YOGESHVEDPATHAK333 <yogeshvedpathak333@gmail.com>
2017-01-26 18:38:55 +0530

    " Changes In Form to Excel "

---
+++ Test1/Test/Microsoft/Office.cs
@@ -0,0 +1,25 @@
+﻿namespace Microsoft
+{
+    internal class Office
+    {
+        internal class Interop
+        {
+            internal class Excel
+            {
+                internal class Application
+                {
+                    internal bool Visible;
+                    internal object Workbooks;
+
+                    public Application()
+                    {
+                    }
+                }
+
+                internal class _Workbook
+                {
+                }
+            }
+        }
+    }
+}
